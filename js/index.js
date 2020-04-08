bde3903a6e9d2dda78d8d0460ad2bc645e4a9a39
Egor Bochkarev <bochkarevei@gmail.com>
2020-04-09 00:54:08 +0300

    remove widget loader

--- js/index.js
+++ js/index.js
@@ -1,10 +1,1 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const multimediaScript = document.createElement('script');
-    multimediaScript.setAttribute('src','./js/multimedia.js');
-    const buttonsContainer = document.querySelector('.buttons-wrap');
-
-    buttonsContainer.innerHTML = '<button class="button button_yellow" type="button">Да</button>' +
-        '<button class="button">Нет</button>';
-    
-    document.querySelector('.multimedia').appendChild(multimediaScript);
-});
+window.addEventListener('DOMContentLoaded',()=>{document.querySelector('.buttons-wrap').innerHTML='<button class="button button_yellow" type="button">Да</button><button class="button">Нет</button>';const multimediaScript=document.createElement('script');multimediaScript.setAttribute('src','./js/multimedia.js');document.querySelector('.multimedia').appendChild(multimediaScript)})
--- js/multimedia.js
+++ js/multimedia.js
@@ -1,15 +1,1 @@
-(function(w, d, n) {
-    var s = d.createElement('script');
-    s.src = '//yastatic.net/s3/zen-lib/widget-loader/widget-loader.ver_2.js';
-    d.head.appendChild(s);
-    var c = d.createElement('div');
-    c.style.display = 'none';
-    d.body.appendChild(c);
-    w[n] = w[n] || [];
-    w[n].push(function() {
-     w.YandexZen.renderWidget({
-      container: c,
-      clid: [123,123],
-     });
-    })
-})(window, document, 'yandexZenAsyncCallbacks')
+(function(w,d,n){var c=d.createElement('div');c.style.display='none';d.body.appendChild(c)})(window,document)
