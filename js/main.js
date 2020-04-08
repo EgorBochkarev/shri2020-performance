187eac9b950c144e8f8572c5eedc9fd9555140e7
Egor Bochkarev <bochkarevei@gmail.com>
2020-04-08 20:13:06 +0300

    Remove usless code from main.js

--- js/main.js
+++ js/main.js
@@ -2,17 +2,6 @@
 
 !function(t) {
     var e = window["pcodeJsonp6187"];
-    window["pcodeJsonp6187"] = function(i, o, r) {
-        for (var a, s, l = 0, p = []; l < i.length; l++)
-            s = i[l],
-            n[s] && p.push(n[s][0]),
-                n[s] = 0;
-        for (a in o)
-            Object.prototype.hasOwnProperty.call(o, a) && (t[a] = o[a]);
-        for (e && e(i, o, r); p.length; )
-            p.shift()()
-    }
-    ;
     var i = {}
         , n = {
         2: 0
@@ -29,76 +18,9 @@
             n.l = !0,
             n.exports
     }
-    o.e = function(t) {
-        var e = n[t];
-        if (0 === e)
-            return new Promise(function(t) {
-                    t()
-                }
-            );
-        if (e)
-            return e[2];
-        var i = new Promise(function(i, o) {
-                e = n[t] = [i, o]
-            }
-        );
-        e[2] = i;
-        var r = document.getElementsByTagName("head")[0]
-            , a = document.createElement("script");
-        a.type = "text/javascript",
-            a.charset = "utf-8",
-            a.async = !0,
-            a.timeout = 12e4,
-        o.nc && a.setAttribute("nonce", o.nc),
-            a.src = o.p + "" + {
-                0: "9b10f65b0c7e7a1d8693",
-                1: "7641180249a9d7be3cfa"
-            }[t] + ".js";
-        var s = setTimeout(l, 12e4);
-        function l() {
-            a.onerror = a.onload = null,
-                clearTimeout(s);
-            var e = n[t];
-            0 !== e && (e && e[1](new Error("Loading chunk " + t + " failed.")),
-                n[t] = void 0)
-        }
-        return a.onerror = a.onload = l,
-            r.appendChild(a),
-            i
-    }
-        ,
         o.m = t,
         o.c = i,
-        o.d = function(t, e, i) {
-            o.o(t, e) || Object.defineProperty(t, e, {
-                configurable: !1,
-                enumerable: !0,
-                get: i
-            })
-        }
-        ,
-        o.n = function(t) {
-            var e = t && t.__esModule ? function() {
-                    return t["default"]
-                }
-                : function() {
-                    return t
-                }
-            ;
-            return o.d(e, "a", e),
-                e
-        }
-        ,
-        o.o = function(t, e) {
-            return Object.prototype.hasOwnProperty.call(t, e)
-        }
-        ,
         o.p = "//an.yandex.ru/partner-code-bundles/6187/",
-        o.oe = function(t) {
-            throw console.error(t),
-                t
-        }
-        ,
         o(o.s = 271)
 }([function(t, e, i) {
     "use strict";
@@ -115,58 +37,18 @@
             , p = i(108)
             , c = r.createComponent([a, l], {
             encodeStyles: null,
-            onInit: function() {
-                this.b_ = s(this.name),
-                    this.mod = this.props.mod || this.mod;
-                var t = this.getContextComponent();
-                this.encodeStyles = Boolean(t && t.encodeStyles || this.props.encodeStyles)
-            },
-            unmount: function() {
-                this.unbindAll(),
-                    this.__base()
-            },
-            pushStyles: function(t, e, i) {
-                p.push(t, e, i)
-            },
-            render: function() {
-                var t = this._render(this.b_, this.props, this.__self);
-                return this.rootComponent = t,
-                    t
-            },
-            _render: function() {
-                return this.getChildren()
-            },
-            getElement: function() {
-                return this.rootComponent.getElement()
-            },
-            bind: function(t, e, i, n) {
-                this._domListeners || (this._domListeners = []);
-                var r = o.domEvent.on(t, e, i, this, n);
-                return this._domListeners.push({
-                    eventType: e,
-                    fn: i,
-                    descriptor: r
-                }),
-                    r
-            },
-            unbindAll: function() {
-                this._domListeners && (o.each(this._domListeners, function(t) {
-                    o.domEvent.un(t.descriptor)
-                }),
-                    delete this._domListeners)
-            },
-            setMod: function(t) {
-                this.rootComponent.addClass(this.b_(t))
-            },
-            delMod: function(t) {
-                this.rootComponent.removeClass(this.b_(t))
-            },
-            addClass: function(t) {
-                this.rootComponent.addClass(t)
-            },
-            removeClass: function(t) {
-                this.rootComponent.removeClass(t)
-            }
+            onInit: function() {},
+            unmount: function() {},
+            pushStyles: function(t, e, i) {},
+            render: function() {},
+            _render: function() {},
+            getElement: function() {},
+            bind: function(t, e, i, n) {},
+            unbindAll: function() {},
+            setMod: function(t) {},
+            delMod: function(t) {},
+            addClass: function(t) {},
+            removeClass: function(t) {}
         }, {
             HTML: n.Html,
             Container: r.components.container,
@@ -218,11 +100,7 @@
             error: a(),
             remoteLog: n.logger.info,
             remoteLogString: n.logger.info,
-            isolate: function(t) {
-                return o("isolate", t || r, this, function(t) {
-                    t.preventProtect = !0
-                })
-            },
+            isolate: function(t) {},
             protect: o,
             setProtectedTimeout: o.setTimeout
         };
