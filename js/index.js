fae2e3065f11660fa3906d02b47ec1c35d1f79d3
Egor Bochkarev <bochkarevei@gmail.com>
2020-04-10 20:08:21 +0300

    remove index.js script

--- js/index.js
+++
@@ -1,1 +0,0 @@
-window.addEventListener('DOMContentLoaded',()=>{document.querySelector('.buttons-wrap').innerHTML='<button class="button button_yellow" type="button">Да</button><button class="button">Нет</button>';const multimediaScript=document.createElement('script');multimediaScript.setAttribute('src','./js/multimedia.js');document.querySelector('.multimedia').appendChild(multimediaScript)})
